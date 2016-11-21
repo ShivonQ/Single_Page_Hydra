ef55c0c733be3be4148766274a895d02d6e81d86
Mason Elmore <mason.elmore@gmail.com>
2016-11-21 11:51:32 -0600

    Add basic search functionality

--- single_page_hydra/app.py
+++ single_page_hydra/app.py
@@ -4,7 +4,11 @@ from flask import (
     request,
 )
 
+from single_page_hydra.api.manager import ApiManager
+
+
 app = Flask(__name__)
+api_manager = ApiManager()
 
 
 @app.route('/')
@@ -17,6 +21,12 @@ def search():
     search_query = request.form.get('search_query')
     return render_template('search.html', query=search_query)
 
+
+@app.route('/search_results', methods=['POST'])
+def search_results():
+    search_query = request.form.get('query')
+    results = api_manager.search(search_query)
+    return render_template('search_results.html', results=results)
 
 if __name__ == '__main__':
     app.run(debug=True)
