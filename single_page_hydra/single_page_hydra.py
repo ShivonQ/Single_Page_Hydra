96906579c2fbc6d1782c56c35655404be80f5c28
Mason Elmore <mason.elmore@gmail.com>
2016-11-21 10:48:57 -0600

    Add routes for the index and search pages

--- single_page_hydra/single_page_hydra.py
+++ single_page_hydra/single_page_hydra.py
@@ -1,12 +1,22 @@
-from flask import Flask
+from flask import (
+    Flask,
+    render_template,
+    request,
+)
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    search_query = request.form.get('search_query')
+    return render_template('search.html', query=search_query)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
